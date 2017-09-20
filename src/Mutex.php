cdc424fa2144c3d9e601ac10faef06a8b9119d50
Denis Mitrofanov <denis.mitr@gmail.com>
2017-09-20 13:12:46 +0300

    Initial

---
+++ src/Mutex.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Denismitr\Mutex;
+
+class Mutex
+{
+
+}
---
+++ test/MutexTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Test;
+
+use Denismitr\Mutex\Mutex;
+use PHPUnit\Framework\TestCase;
+
+class MutexTest extends TestCase
+{
+    /** @test */
+    public function it_can_be_instantiated()
+    {
+        $mutex = new Mutex();
+
+        $this->assertInstanceOf(Mutex::class, $mutex);
+    }
+}
